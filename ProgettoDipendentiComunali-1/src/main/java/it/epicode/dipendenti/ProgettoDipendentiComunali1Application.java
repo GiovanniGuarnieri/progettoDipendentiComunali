d274295678ba765d44d1e1480801ab7d2d0a0277
Francesco Donati <donatifrancesco13@gmail.com>
2022-06-06 18:26:40 +0200

    Changed the name of package

--- ProgettoDipendentiComunali-1/src/main/java/it/epicode/dipendenti/ProgettoDipendentiComunali1Application.java
+++ ProgettoDipendentiComunali-1/src/main/java/it/epicode/dipendenti/ProgettoDipendentiComunali1Application.java
@@ -1,4 +1,4 @@
-package it.epicode.Dipendenti;
+package it.epicode.dipendenti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
