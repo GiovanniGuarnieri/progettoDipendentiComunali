0097e0c62d114d6180721a49385709d2a61c9b68
Giovanni <Giovanni@DESKTOP-Q1Q9MQL.homenet.telecomitalia.it>
2022-06-06 14:53:36 +0200

    inizializzazione progetto

---
+++ ProgettoDipendentiComunali-1/src/main/java/it/epicode/Dipendenti/ProgettoDipendentiComunali1Application.java
@@ -0,0 +1,13 @@
+package it.epicode.Dipendenti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoDipendentiComunali1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoDipendentiComunali1Application.class, args);
+	}
+
+}
---
+++ ProgettoDipendentiComunali-1/src/test/java/it/epicode/Dipendenti/ProgettoDipendentiComunali1ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.Dipendenti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoDipendentiComunali1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
